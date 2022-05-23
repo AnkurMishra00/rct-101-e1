bf51756513389649223c739fcb1b64474adacc7d
Ankur <ankurmishra710@gmail.com>
2022-05-23 16:45:00 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import './App.css';
+import AddTask from "./components/AddTask/AddTask";
+
 
 function App() {
-  return <div>{/* Code Here */}</div>;
+  return <div className="App">{<h1>TODO App with Item Count</h1>}
+  <div className="Second">
+    <div><AddTask  /></div>
+    
+
+  </div>
+  
+  </div>;
+  
 }
 
 export default App;
--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import Counter from "../Counter/Counter";
 import styles from "./addTask.module.css";
 
 const AddTask = () => {
-  // NOTE: do not delete `data-cy` key value pair
+  let [value,setvalue] = React.useState("")
+  const [addtodo,setAddtodo] = React.useState(["jfjl","iuyiyi"])
+  
   return (
     <div className={styles.todoForm}>
-      <input data-cy="add-task-input" type="text" />
-      <button data-cy="add-task-button"></button>
+      <input data-cy="add-task-input" type="cheked" value= {value}
+      onChange = {(e) =>{
+        setvalue(e.target.value)
+      }} />
+      <button data-cy="add-task-button" onClick={() =>{
+        setAddtodo([...addtodo, {id: Date.now(), value: value},
+        ]);
+        setvalue("");
+      }}
+      >+</button>
+      <Counter />
+      
     </div>
   );
 };
